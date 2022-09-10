7ddefa9024041cc2e764b76a4e825121c85e7ce3
dnzbrkyrk <denizberkyurek@yahoo.com>
2022-09-10 20:28:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,20 @@
-resource "azurerm_resource_group" "apimanagement" {
-  name     = "api-management"
-  location = "West Europe"
-}
-
-resource "azurerm_api_management" "apimanagement" {
-  name                = "example-apim"
-  location            = azurerm_resource_group.apimanagement.location
-  resource_group_name = azurerm_resource_group.apimanagement.name
-  publisher_name      = "My Company"
-  publisher_email     = "denizberkyurek4@hotmail.com"
-
-  sku_name = "Developer_1"
-}
-
-resource "azurerm_api_management_api" "apimanagement" {
-  name                = "express-api"
-  resource_group_name = azurerm_resource_group.apimanagement.name
-  api_management_name = azurerm_api_management.apimanagement.name
-  revision            = "1"
-  display_name        = "Express API"
-  path                = "express"
-  protocols           = ["https"]
-
-  import {
-    content_format = "swagger-link-json"
-    content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
   }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
