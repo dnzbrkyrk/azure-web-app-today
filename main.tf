147f885ee0bbb5bdb5976b59e0e6d2d15c696ace
dnzbrkyrk <denizberkyurek@yahoo.com>
2022-09-10 21:56:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_api_management_api" "expressapi" {
      content_value  = "https://raw.githubusercontent.com/dnzbrkyrk/azure-web-app-today/master/Api/azure-web-app-today.swagger.json"
    }
 }
-output "expressapirevision" {
+output "expressapi" {
   description = "Express API Revision"
   value       = expressapi.revision 
 }
