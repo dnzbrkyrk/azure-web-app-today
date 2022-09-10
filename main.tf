36cf9e7558b2b61118f3a9d285652bd228c60872
dnzbrkyrk <denizberkyurek@yahoo.com>
2022-09-10 20:26:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
-provider "aws" {
-    region = "us-west-1"
+resource "azurerm_resource_group" "apimanagement" {
+  name     = "api-management"
+  location = "West Europe"
 }
- 
-resource "aws_instance" "myec2" {
-    ami = "ami-12345qwert"
-    instance_type = "t2.micro"
+
+resource "azurerm_api_management" "apimanagement" {
+  name                = "example-apim"
+  location            = azurerm_resource_group.apimanagement.location
+  resource_group_name = azurerm_resource_group.apimanagement.name
+  publisher_name      = "My Company"
+  publisher_email     = "denizberkyurek4@hotmail.com"
+
+  sku_name = "Developer_1"
+}
+
+resource "azurerm_api_management_api" "apimanagement" {
+  name                = "express-api"
+  resource_group_name = azurerm_resource_group.apimanagement.name
+  api_management_name = azurerm_api_management.apimanagement.name
+  revision            = "1"
+  display_name        = "Express API"
+  path                = "express"
+  protocols           = ["https"]
+
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
+  }
 }
