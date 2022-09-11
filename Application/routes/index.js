edd4009f553e622eb19d61264a10f97dff699d84
deniz <denz@Denizs-MacBook-Pro-2.local>
2022-09-11 11:31:29 +0200

    Update API

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.send({"greeting": "Hello Hello"})
+    res.send({"greetings": "Hello Hello"})
 });
 
 module.exports = router;
